741f1944a2f585611dd003dd251482b75e9b6fb4
Acolit <82833189+Xareks@users.noreply.github.com>
2023-01-13 18:12:43 +0400

    Add files via upload

---
+++ AVTR/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.design__slider').slick({
+        prevArrow: '<button type="button" class="arrow arrow-left"><img src="images/arrow-left.swg" alt=""></button>',
+        nextArrow: '<button type="button" class="arrow arrow-right"><img src="images/arrow-right.swg" alt=""></button>',
+        slidesToShow: 4,
+        dots: false,
+        variableWidth: true,
+    });
+
+});
